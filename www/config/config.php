ed588f6804b58c1203c9cc3833fac7e4b6fdd0b4
Andreas Barth <blackwinged@online.de>
2024-11-03 22:35:53 +0100

    erste API Anbindung

--- www/config/config.php
+++ www/config/config.php
@@ -1,10 +1,21 @@
 <?php
 
-  define("MYSQL_USER", "");
+  define("MYSQL_USER", "root");
   define("MYSQL_PASS", "");
   define("MYSQL_HOST", "localhost");
-  define("MYSQL_DB", "");
+  define("MYSQL_DB", "test");
+  define("MYSQL_TABLE", "heimatradar");
+  define("ADMIN_USER", "admin");
+  define("ADMIN_PASS", "Pass123");
+  define("API_SECRET", "hgfhj2d94x3TsMycgdG4Sxck");
+  define("GOOGLE_MAPS_API_KEY", "AIzaSyD03t0MxCm2i3OG8Ve54h60i45RfUIEgQ8");
 
+  /** Postleitzahl und Ort, an dem sich alle Stände befinden */
+  define("POSTLEITZAHL", "56566");
+  define("ORT", "Heimbach-Weis");
+
+  // wie viele Stände können maximal an einer Adresse registriert werden?
+  define("MAX_HAUSHALTE_PRO_ADRESSE", 10);
 
   $languages = array(
     "de" => "Deutsch",
@@ -27,4 +38,11 @@
       die("Connection failed: " . $e->getMessage());
   }
 
+  $i18n = json_decode(file_get_contents("i18n/de.json"));
+
+  session_start();
+  $isLoggedIn = ((isset($_SESSION["API_SECRET"])) && $_SESSION["API_SECRET"] == API_SECRET);
+
+  define("HEIMATRADAR_INITIALIZED", true);
+  
 ?>
