0a64401043198bcc4ad6a053cf2957a919c39bae
Andreas Barth <blackwinged@online.de>
2024-12-24 21:24:10 +0100

    fixed conflicting css behavior and improved caching

--- www/registrierung.php
+++ www/registrierung.php
@@ -7,11 +7,30 @@
 <html lang="de">
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <!-- Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css">
 
+    <!-- LeafletJS | https://leafletjs.com/ -->  
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
+
+    <!-- LeafletJS Extra Markers | -->
+    <link rel="stylesheet" href="https://www.unpkg.com/leaflet-extra-markers@1.2.2/dist/css/leaflet.extra-markers.min.css" />
+    <script src='https://www.unpkg.com/leaflet-extra-markers@1.2.2/dist/js/leaflet.extra-markers.min.js'></script>
+
+    <!-- Semantic UI -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css" />
+
+    <!-- Fullscreen AddIn | https://github.com/Leaflet/Leaflet.fullscreen -->
+    <script src='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/Leaflet.fullscreen.min.js'></script>
+    <link href='https://api.mapbox.com/mapbox.js/plugins/leaflet-fullscreen/v1.0.1/leaflet.fullscreen.css' rel='stylesheet' />
+  
+    <!-- GPS AddIn | https://github.com/stefanocudini/leaflet-gps -->
+    <script src='/inc/leaflet-gps/leaflet-gps.min.js'></script>
+    <link href='/inc/leaflet-gps/leaflet-gps.min.css' rel='stylesheet' />
+    
   <!-- HeimatRadar -->
   <link href='/inc/heimatradar/heimatradar.css' rel='stylesheet' />
   <script src='/inc/heimatradar/heimatradar.js'></script>
