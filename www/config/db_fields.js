9e31b4a0382931b7b70833594a66c3ca579e5e15
Andreas Barth <blackwinged@online.de>
2025-01-22 21:39:00 +0100

    initial integration of pocketbase, picocss and tabulator

---
+++ www/config/db_fields.js
@@ -0,0 +1,7 @@
+const db_fields = [
+    {title:"Straße", field:"name", sorter:"string"},
+    {title:"Hausnummer", field:"hausnummer", sorter:"number"},
+    {title:"Angebot", field:"angebot", sorter:"string"},
+    {title:"Zahl der Stände", field:"anzahl", sorter:"number"},
+    {title:"GeoJson", field:"geojson", sorter:"string", visible:false}
+]
